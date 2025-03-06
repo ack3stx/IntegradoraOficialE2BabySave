6508bae7cc8cf933a44f335aa2d7b736d6c1f4fe
victor-2222-jpg <vhernandezchavez71@gmail.com>
2025-03-06 17:20:35 -0600

    Tabla admin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -16,6 +16,7 @@ import { CodeFieldComponent } from './Views/auth/code-field/code-field.component
 import { PerfilComponent } from './Views/auth/perfil/perfil.component';
 import { RealtimechartsComponent } from './Views/real_time_charts/realtimecharts/realtimecharts.component';
 import { PanelComponent } from './Views/Admin/panel/panel.component';
+import { CuentasComponent } from './Views/Admin/cuentas/cuentas.component';
 
 export const routes: Routes = [
     {
@@ -94,6 +95,10 @@ export const routes: Routes = [
     {
         path: 'admin',
         component: PanelComponent
+    },
+    {
+        path: 'admin/users',
+        component: CuentasComponent
     }
     /* {
         canMatch: [rutasGuard],
