14568cf10c32e3c38aa82dc05a0bdf44c514441f
victor-2222-jpg <vhernandezchavez71@gmail.com>
2025-03-07 16:21:00 -0600

    inyeccion del modulo de gestion de usuarios

---
+++ src/app/core/services/cuentas/cuentas.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import {Usuario} from '../../models/usuario';
+import { map } from 'rxjs/operators';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CuentasService {
+
+  private ruta = environment.apiUrl + '/todos';
+  
+  
+
+  constructor(private http:HttpClient) { }
+
+  //Obtener todas las cuentas
+   obtenerCuentas() {
+    return this.http.get<Usuario[]>(this.ruta).pipe(
+      map(users => users.filter(user => user.rol_id !== 3))
+    );
+  }
+
+  desactivarCuenta(id: number) {
+    this.ruta = environment.apiUrl + '/desactivar';
+    return this.http.get<any>(`${this.ruta}/${id}`, {});
+  }
+
+}
