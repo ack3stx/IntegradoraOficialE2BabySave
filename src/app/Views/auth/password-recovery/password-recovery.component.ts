3b0e381e35163a7dc448b021634eb649e7e4d8de
OswiiNunez <nunezoswaldo199@gmail.com>
2025-04-02 20:05:46 -0600

    aver q sale 7w7

--- src/app/Views/auth/password-recovery/password-recovery.component.ts
+++ src/app/Views/auth/password-recovery/password-recovery.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-password-recovery',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './password-recovery.component.html',
   styleUrl: './password-recovery.component.css'
 })
