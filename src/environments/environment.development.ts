57b4a8733dfd97013872277786c1ee1ba01faa39
DanSoryu <162482741+DanSoryu@users.noreply.github.com>
2025-04-03 11:36:03 -0600

    hola soy mateo

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -6,5 +6,5 @@ import { Environment } from "../app/core/models/environment";
 
 export const environment : Environment = {
     production: false,
-    apiUrl: 'http://127.0.0.1:8000/api'
+    apiUrl: 'https://babysave.site/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 import { Environment } from "../app/core/models/environment";
 
 export const environment : Environment = {
-    production: false,
-    apiUrl: 'http://127.0.0.1:8000/api'
+    production: true,
+    apiUrl: 'https://babysave.site/api'
 };
